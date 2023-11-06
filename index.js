df241b89cd9320de2e37fcedd9541e25fff5e524
hossainAbirr <hossainabirbusiness@gmail.com>
2023-11-06 21:03:35 +0600

    add product api added

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port =process.env.PORT || 2500;
+const port = process.env.PORT || 2500;
 
 // middlwares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) =>{
-    res.send('Restaurant management server is running')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mongodbexploring.ykpstem.mongodb.net/?retryWrites=true&w=majority`;
 
-app.listen(port, () =>{
-    console.log(`Restaurant management is runnning on: ${port}`);
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const foodCollection = client.db('RestaurantDB').collection('foods')
+
+    
+
+
+    app.post('/addfood', async(req, res) => {
+        const food = req.body;
+        const result = await foodCollection.insertOne(food)
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Restaurant management server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Restaurant management is runnning on: ${port}`);
+});
